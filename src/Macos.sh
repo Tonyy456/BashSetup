ab34f7b13ae6b121409b8c2e74bb8cb1b191d855
Tonyy456 <ajdalesandro0115@gmail.com>
2023-09-06 19:35:59 -0400

    polishing out macos version

--- src/Macos.sh
+++ src/Macos.sh
@@ -1,6 +1,20 @@
+debug_counter=0
+debug() {
+  ((debug_counter++))
+  echo -e "(\033[0;31m$debug_counter\033[0m) $1"
+}
+
 echo "Welcome Macos"
 
 # set ~ directory to user specified directory
 # make some sort of file that says what the current user directory is
+echo "===================================="
+echo "Welcome to Tony's linux setup!"
+echo "===================================="
+echo "Please enter your main folder name, name is a good idea:"
+read -p "> " name
+echo ""
+debug "Creating folder at ~/$name"
+
 # update .zshrc to execute /Tony/.zshrc
 # ask user what folders they DONT want to be made (comma seperated list)
