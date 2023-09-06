8880835a4d5508899f7fb0d646e59287edbe840a
Tonyy456 <ajdalesandro0115@gmail.com>
2023-09-06 19:18:09 -0400

    adding print statements

--- Setup.sh
+++ Setup.sh
@@ -8,8 +8,10 @@ sysname="$(uname -s)"
 if [ "$sysname" == "Linux" ] && [ -f /etc/lsb-release ]; then
     # Check for the presence of the lsb-release file, which is common in Ubuntu.
     echo "You are on Ubuntu."
+    source src/Ubuntu.sh
 elif [ "$sysname" == "Darwin" ]; then
     echo "You are on macOS."
+    source src/Macos.sh
 else
     echo "Unsupported system: $sysname"
 fi
---
+++ src/Macos.sh
@@ -0,0 +1,6 @@
+echo "Welcome Macos"
+
+# set ~ directory to user specified directory
+# make some sort of file that says what the current user directory is
+# update .zshrc to execute /Tony/.zshrc
+# ask user what folders they DONT want to be made (comma seperated list)
